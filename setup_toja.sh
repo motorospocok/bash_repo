520595777e5538c76ad9a4946e95b1e07ecccd91
motorospocok <motorospocok@gmail.com>
2020-10-01 09:10:22 +0100

    draft version

---
+++ setup_toja.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Adding alias commands to .moshellrc
+
+echo jarxmldir=/home/shared/ethtoja/jarxml >> .moshellrc
+echo ip_database=/home/shared/ethtoja/sitefiles/ipdatabase >> .moshellrc
+echo alias rstdu run /home/shared/ethtoja/scriptek/rst_DU.mos >>  .moshellrc
+echo alias trset run /home/shared/ethtoja/scriptek/trace_setup.mos >>  .moshellrc
+echo alias sysconlist run /home/shared/ethtoja/scriptek/sysconlist.mos >>  .moshellrc
+echo alias swq run /home/shared/ethtoja/scriptek/query_SW.mos >>  .moshellrc
+echo alias tmon run /home/shared/ethtoja/scriptek/trace_terminal.mos >> .moshellrc
+echo alias dcl run /home/shared/ethtoja/scriptek/declast.mos >> .moshellrc
