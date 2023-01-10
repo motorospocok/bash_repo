93c90404a852e76be6eb7fa4a2abd6cfdaff4fe1
János Tóth <64922795+motorospocok@users.noreply.github.com>
2023-01-10 15:28:08 +0100

    working version 1.0

---
+++ enter_pod.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ -z "$1" ]; then
+			  echo " "
+			  echo usage: enter_pod.sh "<POD name>"
+			  echo ' '
+			  exit
+		  fi
+
+ALMA=$1
+oc exec -it $(oc get pods | awk -v VAR1=$ALMA '$0 ~ VAR1 {print $1}') -c sshd -- bash
